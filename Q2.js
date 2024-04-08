22dba3fc4fea9c4d782cdac7898042785184bdc5
Gyzelle <137728911+Gyzelle01@users.noreply.github.com>
2024-04-08 15:41:51 +0300

    Update Q2.js

--- Q2.js
+++ Q2.js
@@ -1,15 +1,14 @@
 //Write a JavaScript function that accepts two numbers to generate an array between them. For example, if the two numbers are 4 and 7, the output should be [4, 5, 6, 7] and -4 and 7 should return [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7].
 
-function generateArray(num1,num2){
-    return number [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
-    if(-4 === 7 ){
-        break;
+//Generate an array that accepts two numbers to genrate an array between them.
+function generateArray(char1,char2) {
+    let generateArray = [];
+    for(let a = char1; a <= char2; a++){ 
+
+        generateArray.push(a);
     }
-    else if(-4 == 7)
-    {
-        break;
-    }
+    //The return output will be 4 and 7 
+    return generateArray;
 }
-let input = '4,7'
-let output = '4, 5, 6, 7'(input)
-console.log(output);
+console.log(generateArray(4,7));
+console.log(generateArray(-4,7));
